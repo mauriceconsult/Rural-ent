630d7b260a4d0170f80c7927e09910711b9e449d
Maurice Consultant <mainna62@outlook.com>
2024-08-04 12:12:17 +0300

    'page updated'

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,10 @@ import Feed from "@components/Feed";
 const Home = () => (
   <section className="w-full flex-center flex-col">
     <h1 className="head_text text-center">
-      Custom Management Solution
+      Custom Management Solution:
       <br className="max-md:hidden" />
       <span className="green_gradient text-center">
-        Customized Management Solution{" "}
+        your agile management system
       </span>
     </h1>
     <p className="desc text-center">
